a974dbe008e7645bfa201c15910c5e16fd380743
sorentorp <soren.torp@gmail.com>
2021-04-28 13:14:49 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,26 +1,5 @@
 'use strict';
 
-// handle login with portal Tabulex and Tabulex Tea login. Force the use of UNI-login. Fjern eller kommenter alle linjerne ud til "handle Youtube cookies", hvis denne ikke ønskes. 
-
-chrome.webRequest.onBeforeSendHeaders.addListener(    
-    function(details) {
-          for (var i = 0; i < details.requestHeaders.length; ++i) {
-            if(details.requestHeaders[i].name === 'Cookie') {
-                
-              details.requestHeaders[i].value = details.requestHeaders[i].value.replace ('idpdisco_istidpdisco_lastidp=https%3A%2F%2Fadfs.aarhuskommune.dk%2Fadfs%2Fservices%2Ftrust','idpdisco_istidpdisco_lastidp=https%3A%2F%2Fatlas.uni-login.dk%2Fsimplesaml%2Fsaml2%2Fidp%2Fmetadata.php')
-                                
-                
-//             details.requestHeaders[i].value = '';
-              break;
-             }
-          }
-          return {requestHeaders: details.requestHeaders};
-        },
-        {urls: ["https://saml.personale.tabulex.dk/simplesaml/*",
-        "https://fravaer.tabulex.net/"]},
-    
-        ["blocking", "requestHeaders", "extraHeaders"]);
-
 // handle Youtube cookies. 
 // When using Youtube without permission to log in, playing videoes can sometimes cause problems.
 
